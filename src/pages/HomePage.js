91f090c8b2ad1e9f856b4dc17e60081e0aee8f71
Mahfuz Rahman <mahfuzrahman517@gmail.com>
2023-11-19 03:02:22 +0600

    upto end all requirement

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Layout from '../components/Layout/Layout'
-import axios from 'axios';
 import { Checkbox, Radio } from "antd";
-import { Prices } from '../components/Prices';
+import { Prices } from "../components/Prices";
+import { useCart } from "../context/cart";
+import axios from "axios";
+import toast from "react-hot-toast";
+import Layout from "./../components/Layout/Layout";
 import { AiOutlineReload } from "react-icons/ai";
-
+import "../styles/Homepage.css";
 
 const HomePage = () => {
     const navigate = useNavigate();
+    const [cart, setCart] = useCart();
     const [products, setProducts] = useState([]);
     const [categories, setCategories] = useState([]);
     const [checked, setChecked] = useState([]);
@@ -17,9 +20,7 @@ const HomePage = () => {
     const [page, setPage] = useState(1);
     const [loading, setLoading] = useState(false);
 
-
-
-    //get all category
+    //get all cat
     const getAllCategory = async () => {
         try {
             const { data } = await axios.get("/api/v1/category/get-category");
@@ -28,7 +29,6 @@ const HomePage = () => {
             }
         } catch (error) {
             console.log(error);
-
         }
     };
 
@@ -49,8 +49,6 @@ const HomePage = () => {
         }
     };
 
-
-
     //getTOtal COunt
     const getTotal = async () => {
         try {
@@ -65,7 +63,6 @@ const HomePage = () => {
         if (page === 1) return;
         loadMore();
     }, [page]);
-
     //load more
     const loadMore = async () => {
         try {
@@ -109,12 +106,19 @@ const HomePage = () => {
             console.log(error);
         }
     };
-
     return (
-        <Layout title={'All Products-Best offers'}>
-            <div className="container-fluid row mt-3">
-                <div className="col-md-2">
-                    <h4 className='text-center'>Filter By Category</h4>
+        <Layout title={"ALl Products - Best offers "}>
+            {/* banner image */}
+            <img
+                src="/images/banner.png"
+                className="banner-img"
+                alt="bannerimage"
+                width={"100%"}
+            />
+            {/* banner image */}
+            <div className="container-fluid row mt-3 home-page">
+                <div className="col-md-3 filters">
+                    <h4 className="text-center">Filter By Category</h4>
                     <div className="d-flex flex-column">
                         {categories?.map((c) => (
                             <Checkbox
@@ -126,7 +130,7 @@ const HomePage = () => {
                         ))}
                     </div>
                     {/* price filter */}
-                    <h4 className='text-center mt-4'>Filter By Price</h4>
+                    <h4 className="text-center mt-4">Filter By Price</h4>
                     <div className="d-flex flex-column">
                         <Radio.Group onChange={(e) => setRadio(e.target.value)}>
                             {Prices?.map((p) => (
@@ -145,28 +149,52 @@ const HomePage = () => {
                         </button>
                     </div>
                 </div>
-
-                <div className="col-md-9">
-
-                    <h1 className='text-center'>All Products</h1>
+                <div className="col-md-9 ">
+                    <h1 className="text-center">All Products</h1>
                     <div className="d-flex flex-wrap">
                         {products?.map((p) => (
-
-                            <div className="card m-2" style={{ width: "18rem" }}>
+                            <div className="card m-2" key={p._id}>
                                 <img
                                     src={`/api/v1/product/product-photo/${p._id}`}
                                     className="card-img-top"
                                     alt={p.name}
                                 />
                                 <div className="card-body">
-                                    <h5 className="card-title">{p.name}</h5>
-                                    <p className="card-text">{p.description.substring(0, 30)}</p>
-                                    <p className="card-text"> $ {p.price}</p>
-                                    <button className="btn btn-primary ms-1" onClick={() => navigate(`/product/${p.slug}`)}>More Details</button>
-                                    <button className="btn btn-secondary ms-1">Add To Cart</button>
+                                    <div className="card-name-price">
+                                        <h5 className="card-title">{p.name}</h5>
+                                        <h5 className="card-title card-price">
+                                            {p.price.toLocaleString("en-US", {
+                                                style: "currency",
+                                                currency: "USD",
+                                            })}
+                                        </h5>
+                                    </div>
+                                    <p className="card-text ">
+                                        {p.description.substring(0, 60)}...
+                                    </p>
+                                    <div className="card-name-price">
+                                        <button
+                                            className="btn btn-info ms-1"
+                                            onClick={() => navigate(`/product/${p.slug}`)}
+                                        >
+                                            More Details
+                                        </button>
+                                        <button
+                                            className="btn btn-dark ms-1"
+                                            onClick={() => {
+                                                setCart([...cart, p]);
+                                                localStorage.setItem(
+                                                    "cart",
+                                                    JSON.stringify([...cart, p])
+                                                );
+                                                toast.success("Item Added to cart");
+                                            }}
+                                        >
+                                            ADD TO CART
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
-
                         ))}
                     </div>
                     <div className="m-2 p-3">
@@ -192,7 +220,7 @@ const HomePage = () => {
                 </div>
             </div>
         </Layout>
-    )
-}
+    );
+};
 
-export default HomePage
+export default HomePage;
